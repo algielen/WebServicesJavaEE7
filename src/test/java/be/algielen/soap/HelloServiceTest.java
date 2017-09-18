9dcdc200e7e4646c6bce15e74f1228e390990d8e
Alexandre Gielen <alexandre.gielen@sfpd.fgov.be>
2017-09-18 10:41:41 +0200

    Add JUnit 5

---
+++ src/test/java/be/algielen/soap/HelloServiceTest.java
@@ -0,0 +1,14 @@
+package be.algielen.soap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HelloServiceTest {
+
+    @Test
+    void sayHello() {
+        assertTrue(true);
+    }
+
+}
